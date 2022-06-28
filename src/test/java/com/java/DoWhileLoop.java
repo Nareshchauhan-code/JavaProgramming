18437356fd1eefe396a465292a6886381a2613b2
Naresh Chauhan <62231215+Nareshchauhan-code@users.noreply.github.com>
2022-06-28 18:40:02 +0530

    Done

---
+++ src/test/java/com/java/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        do {
+            System.out.print("* ");
+            i++;
+        } while (i <= 4);
+
+
+    }
+}
